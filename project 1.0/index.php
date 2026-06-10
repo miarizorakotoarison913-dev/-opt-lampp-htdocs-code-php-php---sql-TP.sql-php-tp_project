745a2dec1877e508a6889627b65afbc20a4b8b77
KevinHH <kevinrakoto52@gmail.com>
2026-06-10 15:13:02 +0300

    project 1.0
    new Main

---
+++ project 1.0/index.php
@@ -0,0 +1,23 @@
+
+<!DOCTYPE html>
+<html lang="en">
+<head>
+    <meta charset="UTF-8">
+    <meta name="viewport" content="width=device-width, initial-scale=1.0">
+    <title>Document</title>
+    <link rel="stylesheet" href="bootstrap/css/bootstrap.min.css">
+    <link rel="stylesheet" href="bootstrap/font/bootstrap-icons.css">
+    <link rel="stylesheet" href="css/style.css">
+</head>
+<body>
+    <header>
+    <div class="section text-center bg-info">
+    <h1><a href="page/liste.php">LISTE DES DEPARTEMENTS</a></h1>
+    <h1><a href="page/manager.php">LISTE DES MANAGER DANS LES DEPARTEMENTS</a></h1>
+
+    </div>
+    </header>
+ 
+    
+</body>
+</html>
